e639a9e1b7f499124755e01681433b6b227872cb
Yuga Samuel <yugasamuel2002@gmail.com>
2023-12-02 14:46:57 +0700

    Initial Commit

---
+++ GojekAccessibilityReview/GojekAccessibilityReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GojekAccessibilityReviewApp.swift
+//  GojekAccessibilityReview
+//
+//  Created by Yuga Samuel on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GojekAccessibilityReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
